174759a4d659ec44325b7ecb0189b1d667697b21
cjlee38 <cjlee38@github.com>
2022-09-16 20:16:03 +0900

    chore: delete not null for columns which are not necessary

--- src/main/resources/db/migration/V3_2__Add_judge_tables.sql
+++ src/main/resources/db/migration/V3_2__Add_judge_tables.sql
@@ -1,10 +1,10 @@
 create table judge_item
 (
-    id                   bigint       not null auto_increment,
-    mission_id           bigint       not null,
-    evaluation_item_id   bigint       not null,
-    test_name            varchar(255) not null,
-    programming_language varchar(255) not null,
+    id                   bigint not null auto_increment,
+    mission_id           bigint not null,
+    evaluation_item_id   bigint,
+    test_name            varchar(255),
+    programming_language varchar(255),
     primary key (id)
 ) engine = InnoDB
   default charset = utf8mb4;
