cffce61a269c06b42ef1c272e489eabb02ad3195
summerlunaa <summerlunaa9@gmail.com>
2022-09-18 17:12:40 +0900

    feat: add judge real and request function to judge system

--- src/main/kotlin/apply/application/JudgmentService.kt
+++ src/main/kotlin/apply/application/JudgmentService.kt
@@ -1,6 +1,7 @@
 package apply.application
 
 import apply.application.github.GithubApi
+import apply.application.judgmentserver.JudgmentRequest
 import apply.domain.assignment.Assignment
 import apply.domain.assignment.AssignmentRepository
 import apply.domain.assignment.getByUserIdAndMissionId
@@ -8,41 +9,86 @@ import apply.domain.judgment.Commit
 import apply.domain.judgment.Commits
 import apply.domain.judgment.JudgmentHistory
 import apply.domain.judgment.JudgmentHistoryRepository
+import apply.domain.judgment.JudgmentItemRepository
 import apply.domain.judgment.JudgmentType
-import apply.domain.judgment.findLastByUserIdAndMissionId
+import apply.domain.judgment.findLastByUserIdAndMissionIdAndJudgmentType
+import apply.domain.judgment.getByMissionId
+import apply.domain.judgmentserver.JudgmentServer
 import apply.domain.mission.Mission
 import apply.domain.mission.MissionRepository
 import apply.domain.mission.getById
 import org.springframework.stereotype.Service
 
-private const val TEMPORARY_REQUEST_KEY = "temporary-request-key"
-
 @Service
 class JudgmentService(
     private val missionRepository: MissionRepository,
     private val assignmentRepository: AssignmentRepository,
+    private val judgmentItemRepository: JudgmentItemRepository,
     private val judgmentHistoryRepository: JudgmentHistoryRepository,
-    private val githubApi: GithubApi
+    private val githubApi: GithubApi,
+    private val judgmentServer: JudgmentServer
 ) {
     fun judgeExample(missionId: Long, userId: Long): JudgmentHistoryResponse {
-        val mission = missionRepository.getById(missionId)
-        check(mission.isSubmitting) { "예제 테스트 실행 가능 시간이 아닙니다." }
-
-        val assignment = assignmentRepository.getByUserIdAndMissionId(userId, missionId)
-        val latestCommit = findLatestCommitFromGithub(mission, assignment)
-        val lastHistory = judgmentHistoryRepository.findLastByUserIdAndMissionId(userId, missionId)
-
-        // TODO : implement api request to judge system
-        return (
-            lastHistory?.takeIf { it.isCompleted(latestCommit) }
-                ?: JudgmentHistory(userId, missionId, TEMPORARY_REQUEST_KEY, latestCommit.hash, JudgmentType.EXAMPLE)
-            ).let { JudgmentHistoryResponse(it, assignment) }
+        return judge(missionId, userId, JudgmentType.EXAMPLE)
     }
 
-    private fun findLatestCommitFromGithub(mission: Mission, assignment: Assignment): Commit {
-        return githubApi.requestCommits(assignment)
+    fun judgeReal(missionId: Long, userId: Long): JudgmentHistoryResponse {
+        return judge(missionId, userId, JudgmentType.REAL)
+    }
+
+    private fun judge(missionId: Long, userId: Long, judgmentType: JudgmentType): JudgmentHistoryResponse {
+        val mission = missionRepository.getById(missionId)
+        val assignment = assignmentRepository.getByUserIdAndMissionId(userId, missionId)
+        val lastHistory =
+            judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(userId, missionId, judgmentType)
+
+        if (judgmentType == JudgmentType.EXAMPLE) {
+            check(mission.isSubmitting) { "예제 테스트 실행 가능 시간이 아닙니다." }
+        }
+
+        val history = getOrCreateNew(assignment, mission, lastHistory, judgmentType)
+        return JudgmentHistoryResponse(history, assignment.pullRequestUrl)
+    }
+
+    private fun getOrCreateNew(
+        assignment: Assignment,
+        mission: Mission,
+        lastHistory: JudgmentHistory?,
+        judgmentType: JudgmentType
+    ): JudgmentHistory {
+        val latestCommit = findLatestCommit(assignment.pullRequestUrl, mission)
+        if (lastHistory != null && lastHistory.isCompleted(latestCommit)) {
+            return lastHistory
+        }
+
+        val requestKey = requestJudgment(assignment, judgmentType, latestCommit)
+        return judgmentHistoryRepository.save(
+            JudgmentHistory(assignment.userId, assignment.missionId, requestKey, latestCommit.hash, judgmentType)
+        )
+    }
+
+    private fun findLatestCommit(pullRequestUrl: String, mission: Mission): Commit {
+        return githubApi.requestCommits(pullRequestUrl)
             .map { Commit(it.hash, it.date) }
             .let(::Commits)
             .findLatestUntilEndDateTime(mission.period.endDateTime)
+    }
+
+    private fun requestJudgment(
+        assignment: Assignment,
+        judgmentType: JudgmentType,
+        latestCommit: Commit
+    ): String {
+        val item = judgmentItemRepository.getByMissionId(assignment.missionId)
+            .also { if (!it.isValid()) throw IllegalArgumentException("자동채점 항목이 유효하지 않습니다.") }
+        return judgmentServer.requestJudgement(
+            JudgmentRequest(
+                item.testName!!,
+                judgmentType,
+                assignment.pullRequestUrl,
+                latestCommit.hash,
+                item.programmingLanguage!!
+            )
+        )
     }
 }
--- src/test/kotlin/apply/application/JudgmentServiceTest.kt
+++ src/test/kotlin/apply/application/JudgmentServiceTest.kt
@@ -1,15 +1,22 @@
 package apply.application
 
 import apply.COMMIT_HASH
+import apply.PULL_REQUEST_URL
+import apply.REQUEST_KEY
 import apply.application.github.GithubApi
 import apply.createAssignment
 import apply.createCommitResponse
 import apply.createJudgmentHistory
+import apply.createJudgmentItem
 import apply.createMission
 import apply.domain.assignment.AssignmentRepository
 import apply.domain.assignment.getByUserIdAndMissionId
 import apply.domain.judgment.JudgmentHistoryRepository
-import apply.domain.judgment.findLastByUserIdAndMissionId
+import apply.domain.judgment.JudgmentItemRepository
+import apply.domain.judgment.JudgmentType
+import apply.domain.judgment.findLastByUserIdAndMissionIdAndJudgmentType
+import apply.domain.judgment.getByMissionId
+import apply.domain.judgmentserver.JudgmentServer
 import apply.domain.mission.MissionRepository
 import apply.domain.mission.getById
 import io.kotest.assertions.throwables.shouldThrow
@@ -23,11 +30,18 @@ import java.time.LocalDateTime
 class JudgmentServiceTest : BehaviorSpec({
     val missionRepository = mockk<MissionRepository>()
     val assignmentRepository = mockk<AssignmentRepository>()
+    val judgmentItemRepository = mockk<JudgmentItemRepository>()
     val judgmentHistoryRepository = mockk<JudgmentHistoryRepository>()
     val githubApi = mockk<GithubApi>()
+    val judgementServer = mockk<JudgmentServer>()
 
     val judgmentService = JudgmentService(
-        missionRepository, assignmentRepository, judgmentHistoryRepository, githubApi
+        missionRepository,
+        assignmentRepository,
+        judgmentItemRepository,
+        judgmentHistoryRepository,
+        githubApi,
+        judgementServer
     )
 
     Given("과제 제출 시간이 아닌 경우") {
@@ -35,15 +49,36 @@ class JudgmentServiceTest : BehaviorSpec({
             startDateTime = LocalDateTime.now().minusMinutes(2),
             endDateTime = LocalDateTime.now().minusMinutes(1)
         )
-        every { githubApi.requestCommits(any()) } returns listOf(createCommitResponse())
         every { assignmentRepository.getByUserIdAndMissionId(any(), any()) } returns createAssignment()
-        every { judgmentHistoryRepository.findLastByUserIdAndMissionId(any(), any()) } returns null
+        every {
+            judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(any(), any(), any())
+        } returns null
+
 
         When("해당 과제 제출물의 예제 테스트를 실행하면") {
-            Then("예외가 발생한다.") {
+            Then("예외가 발생한다") {
                 shouldThrow<IllegalStateException> {
                     judgmentService.judgeExample(1L, 1L)
                 }
+            }
+        }
+
+        When("해당 과제 제출물의 본 테스트를 실행하면") {
+            every { githubApi.requestCommits(any()) } returns listOf(
+                createCommitResponse(
+                    date = LocalDateTime.now().minusMinutes(3)
+                )
+            )
+            every { judgementServer.requestJudgement(any()) } returns REQUEST_KEY
+            every { judgmentItemRepository.getByMissionId(any()) } returns createJudgmentItem()
+            val createHistory = createJudgmentHistory(judgmentType = JudgmentType.REAL)
+            every { judgmentHistoryRepository.save(any()) } returns createHistory
+
+            Then("정상적으로 채점을 진행한다") {
+                val actual = judgmentService.judgeReal(1L, 1L)
+                val expected = JudgmentHistoryResponse(createHistory, PULL_REQUEST_URL)
+
+                actual shouldBe expected
             }
         }
     }
@@ -54,40 +89,97 @@ class JudgmentServiceTest : BehaviorSpec({
             createCommitResponse(date = LocalDateTime.now().plusDays(8))
         )
         every { assignmentRepository.getByUserIdAndMissionId(any(), any()) } returns createAssignment()
-        every { judgmentHistoryRepository.findLastByUserIdAndMissionId(any(), any()) } returns null
+        every {
+            judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(
+                any(),
+                any(),
+                any()
+            )
+        } returns null
 
         When("해당 과제 제출물의 예제 테스트를 실행하면") {
-            Then("예외가 발생한다.") {
+            Then("예외가 발생한다") {
                 shouldThrow<IllegalArgumentException> {
                     judgmentService.judgeExample(1L, 1L)
+                }
+            }
+        }
+
+        When("해당 과제 제출물의 본 테스트를 실행하면") {
+            Then("예외가 발생한다") {
+                shouldThrow<IllegalArgumentException> {
+                    judgmentService.judgeReal(1L, 1L)
                 }
             }
         }
     }
 
     Given("테스트 실행 이력이 존재하는 경우") {
-        val assignment = createAssignment()
         every { missionRepository.getById(any()) } returns createMission()
         every { githubApi.requestCommits(any()) } returns listOf(createCommitResponse())
-        every { assignmentRepository.getByUserIdAndMissionId(any(), any()) } returns assignment
+        every { assignmentRepository.getByUserIdAndMissionId(any(), any()) } returns createAssignment()
 
         When("제출한 commit id와 최신 이력의 commit id가 같은 예제 테스트를 실행하면") {
             val existHistory = createJudgmentHistory(commitHash = COMMIT_HASH)
-            every { judgmentHistoryRepository.findLastByUserIdAndMissionId(any(), any()) } returns existHistory
+            every {
+                judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(
+                    any(),
+                    any(),
+                    any()
+                )
+            } returns existHistory
 
-            Then("기존의 최신 테스트 실행 결과를 반환한다.") {
+            Then("기존의 최신 테스트 실행 결과를 반환한다") {
                 val executionResponse = judgmentService.judgeExample(1L, 1L)
-                executionResponse shouldBe JudgmentHistoryResponse(existHistory, assignment)
+                executionResponse shouldBe JudgmentHistoryResponse(existHistory, PULL_REQUEST_URL)
             }
         }
 
         When("제출한 commit id와 최신 이력의 commit id가 다른 예제 테스트를 실행하면") {
             val existHistory = createJudgmentHistory(commitHash = "old-commit")
-            every { judgmentHistoryRepository.findLastByUserIdAndMissionId(any(), any()) } returns existHistory
+            every {
+                judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(
+                    any(),
+                    any(),
+                    any()
+                )
+            } returns existHistory
 
-            Then("테스트를 실행한 뒤 새로운 테스트 실행 결과를 반환한다.") {
+            Then("테스트를 실행한 뒤 새로운 테스트 실행 결과를 반환한다") {
                 val executionResponse = judgmentService.judgeExample(1L, 1L)
-                executionResponse shouldNotBe JudgmentHistoryResponse(existHistory, assignment)
+                executionResponse shouldNotBe JudgmentHistoryResponse(existHistory, PULL_REQUEST_URL)
+            }
+        }
+
+        When("제출한 commit id와 최신 이력의 commit id가 같은 본 테스트를 실행하면") {
+            val existHistory = createJudgmentHistory(commitHash = COMMIT_HASH)
+            every {
+                judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(
+                    any(),
+                    any(),
+                    any()
+                )
+            } returns existHistory
+
+            Then("기존의 최신 테스트 실행 결과를 반환한다") {
+                val executionResponse = judgmentService.judgeReal(1L, 1L)
+                executionResponse shouldBe JudgmentHistoryResponse(existHistory, PULL_REQUEST_URL)
+            }
+        }
+
+        When("제출한 commit id와 최신 이력의 commit id가 다른 본 테스트를 실행하면") {
+            val existHistory = createJudgmentHistory(commitHash = "old-commit")
+            every {
+                judgmentHistoryRepository.findLastByUserIdAndMissionIdAndJudgmentType(
+                    any(),
+                    any(),
+                    any()
+                )
+            } returns existHistory
+
+            Then("테스트를 실행한 뒤 새로운 테스트 실행 결과를 반환한다") {
+                val executionResponse = judgmentService.judgeReal(1L, 1L)
+                executionResponse shouldNotBe JudgmentHistoryResponse(existHistory, PULL_REQUEST_URL)
             }
         }
     }
